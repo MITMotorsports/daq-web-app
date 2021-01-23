21fdf00fbb3c305d62b589337890808eca2fc165
Zachary <dengzac@gmail.com>
2021-01-23 13:18:41 -0500

    Rearrange log preview page

--- src/pages/FileModal.tsx
+++ src/pages/FileModal.tsx
@@ -14,14 +14,11 @@ import {
   ListSubheader,
   ListItem,
   Grid,
+  Checkbox,
 } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
 
-import {
-  MapContainer,
-  TileLayer,
-  Polyline,
-} from "react-leaflet";
+import { MapContainer, TileLayer, Polyline } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
@@ -36,6 +33,9 @@ interface FileModalProps {
 
 const FilePreview: React.FC<FileModalProps> = ({ file }) => {
   const [data, setData] = useState<FilePreviewData | null>(null);
+  const [fieldChecked, setFieldChecked] = useState<string[]>([
+    'field1', 'field2'
+  ]);
   useEffect(() => {
     if (!data && file)
       getPreviewData(file)
@@ -46,56 +46,95 @@ const FilePreview: React.FC<FileModalProps> = ({ file }) => {
   if (data)
     return (
       <div>
-        <List subheader={<ListSubheader>Fields</ListSubheader>}>
-          {data.fields_data && data.fields_data.size > 0 ? (
-            Array.from(data.fields_data.keys()).map((x) => (
-              <ListItem><Typography>{x}</Typography></ListItem>
-            ))
-          ) : (
-            <Alert severity="warning">No fields available</Alert>
-          )}
-        </List>
-        <List subheader={<ListSubheader>Info</ListSubheader>}>
-          {data.info?.map(([k, v]) => (
-            <ListItem>
-              <Grid container spacing={3}>
-                <Grid item><Typography>{k}</Typography></Grid>
-                <Grid item><Typography>{v}</Typography></Grid>
-              </Grid>
-            </ListItem>
-          ))}
-        </List>
+        <Grid container justify="flex-start" spacing={2}>
+          <Grid item xs={3}>
+            <List subheader={<ListSubheader>Info</ListSubheader>}>
+              {data.info?.map(([k, v]) => (
+                <ListItem style={{ padding: 0 }}>
+                  <Grid container spacing={3}>
+                    <Grid item>
+                      <Typography>{k}</Typography>
+                    </Grid>
+                    <Grid item>
+                      <Typography>{v}</Typography>
+                    </Grid>
+                  </Grid>
+                </ListItem>
+              ))}
+            </List>
 
-        {data.gps_coords ? (
-          <div style={{ height: "300px", width: "300px" }}>
-            <MapContainer
-              bounds={L.latLngBounds(data.gps_coords)}
-              scrollWheelZoom={false}
-              style={{ position: "static", height: "100%" }}
+            {data.gps_coords ? (
+              <div style={{ height: "300px", width: "300px" }}>
+                <MapContainer
+                  bounds={L.latLngBounds(data.gps_coords)}
+                  scrollWheelZoom={false}
+                  style={{ position: "static", height: "100%" }}
+                >
+                  <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  <Polyline positions={data.gps_coords} />
+                </MapContainer>
+              </div>
+            ) : null}
+          </Grid>
+
+          <Grid item xs>
+            <List
+              subheader={<ListSubheader>Fields</ListSubheader>}
+              style={{ display: "flex", flexDirection: "row" }}
             >
-              <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Polyline positions={data.gps_coords} />
-            </MapContainer>
-          </div>
-        ) : null}
-
-        {data.fields_data
-          ? Array.from(data.fields_data.keys()).map((field) => (
-              <Plot
-                data={[
-                  {
-                    x: data.fields_data!.get(field)![0],
-                    y: data.fields_data!.get(field)![1],
-                    type: "scatter",
-                  },
-                ]}
-                layout={{ title: field }}
-              />
-            ))
-          : null}
+              {data.fields_data && data.fields_data.size > 0 ? (
+                Array.from(data.fields_data.keys()).map((x) => (
+                  <ListItem style={{ padding: 0 }}>
+                    <Checkbox
+                      checked={!!fieldChecked?.find((k) => k === x)}
+                      onChange={(e) => {
+                        let newChecked = [...fieldChecked];
+                        console.log(fieldChecked);
+                        if (e.target.checked) {
+                          newChecked.push(x);
+                        } else {
+                          const index = fieldChecked.findIndex((k) => k === x);
+                          newChecked.splice(index, 1);
+                        }
+                        setFieldChecked(newChecked);
+                      }}
+                    ></Checkbox>
+                    <Typography>{x}</Typography>
+                  </ListItem>
+                ))
+              ) : (
+                <Alert severity="warning">No fields available</Alert>
+              )}
+            </List>
+            <div style={{ flexGrow: 1 }}>
+              <Grid container spacing={0}>
+                {data.fields_data
+                  ? Array.from(data.fields_data.keys()).map((field) =>
+                      !!fieldChecked?.find((k) => k === field) ? (
+                        <Grid item xs>
+                          <Plot
+                            useResizeHandler={true}
+                            data={[
+                              {
+                                x: data.fields_data!.get(field)![0],
+                                y: data.fields_data!.get(field)![1],
+                                type: "scatter",
+                              },
+                            ]}
+                            layout={{ title: field }}
+                            style={{ width: "100%", display: "block" }}
+                          />
+                        </Grid>
+                      ) : null
+                    )
+                  : null}
+              </Grid>
+            </div>
+          </Grid>
+        </Grid>
       </div>
     );
 
@@ -131,7 +170,7 @@ const FileModal: React.FC<FileModalProps> = ({ file }) => {
       <Button disabled={downloadUrl === undefined} onClick={copyToClipboard}>
         Copy Url
       </Button>
-      <Typography>{ copySuccess }</Typography >
+      <Typography>{copySuccess}</Typography>
       <FilePreview file={file}></FilePreview>
     </div>
   );
