75c053e62101efa97fd8eb3d141502b2390851ed
Raymond Huffman <rhuffman@mit.edu>
2021-02-19 15:53:56 -0800

    remove extra space

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import firebase from "firebase/app";
 
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
