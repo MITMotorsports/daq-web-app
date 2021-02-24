ca2e6841fee3dfe8e292fd3a7fc47af279ab7185
Zachary Deng <dengzac@mit.edu>
2021-02-24 14:56:30 -0500

    Change field selection to dropdown

--- src/components/FilePreview.tsx
+++ src/components/FilePreview.tsx
@@ -11,6 +11,9 @@ import {
   Grid,
   Checkbox,
   CircularProgress,
+  Select,
+  ListItemText,
+  MenuItem,
 } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
 
@@ -20,10 +23,7 @@ interface Props {
 
 const FilePreview: React.FC<Props> = ({ file }) => {
   const [data, setData] = useState<FilePreviewData | null>(null);
-  const [fieldChecked, setFieldChecked] = useState<string[]>([
-    "field1",
-    "field2",
-  ]);
+  const [fieldChecked, setFieldChecked] = useState<string[]>([]);
   useEffect(() => {
     if (!data && file)
       getPreviewData(file)
@@ -50,43 +50,38 @@ const FilePreview: React.FC<Props> = ({ file }) => {
                 </ListItem>
               ))}
             </List>
-
-            {data.gps_coords ? <GPSMap coords={data.gps_coords} /> : null}
           </Grid>
 
           <Grid item xs>
-            <List
-              subheader={<ListSubheader>Fields</ListSubheader>}
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                flexWrap: "wrap",
-              }}
-            >
+            <List subheader={<ListSubheader>Fields</ListSubheader>}>
               {data.fields_data && data.fields_data.size > 0 ? (
-                Array.from(data.fields_data.keys()).map((x) => (
-                  <ListItem style={{ padding: 0, width: "auto" }}>
-                    <Checkbox
-                      checked={!!fieldChecked?.find((k) => k === x)}
-                      onChange={(e) => {
-                        let newChecked = [...fieldChecked];
-                        if (e.target.checked) {
-                          newChecked.push(x);
-                        } else {
-                          const index = fieldChecked.findIndex((k) => k === x);
-                          newChecked.splice(index, 1);
-                        }
-                        setFieldChecked(newChecked);
-                      }}
-                    ></Checkbox>
-                    <Typography>{x}</Typography>
-                  </ListItem>
-                ))
+                <Select
+                  value={fieldChecked}
+                  onChange={(e) => setFieldChecked(e.target.value as [])}
+                  renderValue={(selected) => (selected as string[]).join(", ")}
+                  multiple
+                >
+                  {Array.from(data?.fields_data?.keys()).map((v) => (
+                    <MenuItem key={v} value={v}>
+                      <ListItemText>{v}</ListItemText>
+                      <Checkbox
+                        checked={fieldChecked?.indexOf(v) > -1}
+                      ></Checkbox>
+                    </MenuItem>
+                  ))}
+                </Select>
               ) : (
                 <Alert severity="warning">No fields available</Alert>
               )}
             </List>
+          </Grid>
+        </Grid>
 
+        <Grid container>
+          <Grid item>
+            {data.gps_coords ? <GPSMap coords={data.gps_coords} /> : null}
+          </Grid>
+          <Grid item>
             {data.fields_data
               ? (() => {
                   const checked_fields = Array.from(
